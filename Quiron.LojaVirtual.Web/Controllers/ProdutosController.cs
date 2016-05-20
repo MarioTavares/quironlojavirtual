91d1183ce83ae773515dcf2836a89691bf6dd2e3
MarioTavares <suporte@aminformatica.net.br>
2016-05-19 21:39:31 -0300

    aula6

---
+++ Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        //
+        // GET: /Produtos/
+
+        private ProdutosRepositorio _repositorio;
+
+        public ActionResult Index()
+        {
+
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.Take(20);
+
+            return View(produtos);
+
+            
+        }
+    }
+}
