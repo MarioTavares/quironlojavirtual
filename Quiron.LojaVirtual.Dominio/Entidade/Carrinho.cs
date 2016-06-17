1abb2b2871fb432b992ed1633ec6b927b7d73986
MarioTavares <suporte@aminformatica.net.br>
2016-06-16 21:17:43 -0300

    Crrinho

---
+++ Quiron.LojaVirtual.Dominio/Entidade/Carrinho.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+    public class Carrinho
+    {
+
+        private readonly List<ItemCarrinho> _itemCarrinho = new List<ItemCarrinho>();
+
+
+        // Adicionar
+
+        public void AdicionarItem(Produto produto, int quantidade)
+        {
+
+            ItemCarrinho item = _itemCarrinho.FirstOrDefault(p => p.Produto.produtoId == produto.produtoId);
+
+            if (item == null)
+            {
+
+                _itemCarrinho.Add(new ItemCarrinho
+
+                    {
+
+                        Produto = produto,
+                        Quantidade = quantidade
+
+                    });
+            }
+            else
+            {
+
+                item.Quantidade += quantidade;
+
+            }
+
+
+        }
+
+
+        //Remover
+
+        public void RemoverItem(Produto produto)
+        {
+
+            _itemCarrinho.RemoveAll(l => l.Produto.produtoId == produto.produtoId);
+
+
+        }
+
+
+        //Obter valor total
+
+        public decimal ObterValorTotal()
+        {
+
+            return _itemCarrinho.Sum(e => e.Produto.Preco*e.Quantidade);
+        }
+
+
+        //Limpar carrinho
+
+        public void LimparCarrino()
+        {
+
+            _itemCarrinho.Clear();
+        }
+
+        //Itens do carrinho
+
+        public IEnumerable<ItemCarrinho> ItensCarrinho
+        {
+
+            get { return _itemCarrinho; }
+        }
+
+
+    }
+
+
+    public class ItemCarrinho
+    {
+
+        public Produto Produto { get; set; }
+
+        public int Quantidade { get; set; }
+
+    }
+
+
+
+}
