9a2eb929e0e1e0e1f5fb426aec2e6945e184c8f6
MarioTavares <suporte@aminformatica.net.br>
2016-08-08 20:10:47 -0300

    aula42

--- Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -3,24 +3,28 @@ using System.Web.Mvc;
 
 namespace Quiron.LojaVirtual.Dominio.Entidade
 {
- public class Produto
+    public class Produto
     {
 
-     [HiddenInput(DisplayValue = true)]
-     public int ProdutoId { get; set; }
-     [Required(ErrorMessage="Digite o nome do Produto")]
-     public string Nome { get; set; }
-     [DataType(DataType.MultilineText)]
-     [Required(ErrorMessage="Digite a descrição do Produto")]
-     public string Descricao { get; set; }
-     [Required(ErrorMessage="Digite o valor")]
-     [Range(0.01,double.MaxValue,ErrorMessage="Valor inválido")]
-     public  decimal Preco { get; set; }
-     [Required(ErrorMessage="Digite a categoria")]
-     public string Categoria  { get; set; }
+        [HiddenInput(DisplayValue = true)]
+        public int ProdutoId { get; set; }
+        [Required(ErrorMessage = "Digite o nome do Produto")]
+        public string Nome { get; set; }
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Digite a descrição do Produto")]
+        public string Descricao { get; set; }
+        [Required(ErrorMessage = "Digite o valor")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Valor inválido")]
+        public decimal Preco { get; set; }
+        [Required(ErrorMessage = "Digite a categoria")]
+        public string Categoria { get; set; }
+
+        public byte[] Imagem { get; set; }
+
+        public string ImagemMimeType { get; set; }
 
 
-        
+
 
     }
 }
